c197ac8c5f838923c49a63c4c495900e5fdb1bcd
sans123wx <514232900@qq.com>
2018-11-13 22:23:40 +0800

    换成mysql

--- django_learnings/settings.py
+++ django_learnings/settings.py
@@ -73,14 +73,25 @@ WSGI_APPLICATION = 'django_learnings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_learnings_db',
+        'USER': 'root',
+        'PASSWORD': '19890509wx',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
